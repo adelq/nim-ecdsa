76643ee426c0acf5177bcdca602ffe6c61072bed
Adel Qalieh <aqalieh95@gmail.com>
2015-01-09 22:44:17 -0500

    Add tests for elliptic curves (not passing)

--- tests/test_ellipticcurve.nim
+++ tests/test_ellipticcurve.nim
@@ -6,10 +6,26 @@ proc test_add(c, x1, y1, x2, y2, x3, y3) =
   var p2 = Point(curve: c, x: x2, y: y2)
   var p3 = p1 + p2
 
-  if p3.x != x3 or p3.y != y3:
-    echo("Failure")
-  else:
-    echo("Success")
+  test "Test adding":
+    check p3.x == x3 and p3.y == y3
+
+proc test_double(c, x1, y1, x3, y3) =
+  var p1 = Point(curve: c, x: x1, y: y1)
+  var p3 = p1.double
+
+  test "Test doubling":
+    check p3.x == x3 and p3.y == y3
+
+proc test_multiply(c, x1, y1, m, x3, y3) =
+  var p1 = Point(curve: c, x: x1, y: y1)
+  var p3 = p1 * m
+
+  test "Test doubling":
+    check p3.x == x3 and p3.y == y3
 
 var c = CurveFp(p: 23, a: 1, b: 1)
+
 test_add(c, 3, 10, 9, 7, 17, 20)
+test_double(c, 3, 10, 7, 12)
+test_add(c, 3, 10, 3, 10, 7, 12)
+test_multiply(c, 3, 10, 2, 7, 12)
